058af1e57240e05a76287841e98fef3c4949d34b
Tuan Le <shinjnt4@gmail.com>
2020-08-22 21:44:30 +0700

    Your awesome message

---
+++ build/precache-manifest.05759b744568059a0b1647dec0bba7e1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2580f2eabf3fef843f23f67703803077",
+    "url": "/Ecommerce/index.html"
+  },
+  {
+    "revision": "05272df7f6b157f79cc2",
+    "url": "/Ecommerce/static/css/2.3450c87a.chunk.css"
+  },
+  {
+    "revision": "dc40833a675499bb9a6d",
+    "url": "/Ecommerce/static/css/main.e9b1b8f7.chunk.css"
+  },
+  {
+    "revision": "05272df7f6b157f79cc2",
+    "url": "/Ecommerce/static/js/2.269b7bac.chunk.js"
+  },
+  {
+    "revision": "25de31290819161a02990a43be52fc26",
+    "url": "/Ecommerce/static/js/2.269b7bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc40833a675499bb9a6d",
+    "url": "/Ecommerce/static/js/main.2cdc5e36.chunk.js"
+  },
+  {
+    "revision": "65ad8180acdc1e7974b8",
+    "url": "/Ecommerce/static/js/runtime-main.66ad74a6.js"
+  },
+  {
+    "revision": "35133ab364831cff43c977fc147d25f1",
+    "url": "/Ecommerce/static/media/about.35133ab3.jpg"
+  },
+  {
+    "revision": "13d863aba2fd4854390928cd47f3dfc6",
+    "url": "/Ecommerce/static/media/banner.13d863ab.png"
+  },
+  {
+    "revision": "31b0ed7cc69b1af58b61d30e3d159a92",
+    "url": "/Ecommerce/static/media/bg_1.31b0ed7c.jpg"
+  },
+  {
+    "revision": "19695892ac9f2874c1178383ca1346f3",
+    "url": "/Ecommerce/static/media/bg_2.19695892.jpg"
+  },
+  {
+    "revision": "ae53064764eb996f4674f0d02cefdc28",
+    "url": "/Ecommerce/static/media/bg_4.ae530647.jpg"
+  },
+  {
+    "revision": "bf485d677b3ae928da19b54011dadd06",
+    "url": "/Ecommerce/static/media/box.bf485d67.jpg"
+  },
+  {
+    "revision": "9411e170530a2fd89aff276b1d22fa7d",
+    "url": "/Ecommerce/static/media/medal.9411e170.jpg"
+  },
+  {
+    "revision": "bbcd5413ffcc46f7d9eef5fb36340659",
+    "url": "/Ecommerce/static/media/person_1.bbcd5413.jpg"
+  },
+  {
+    "revision": "2cd3dcd36bb7c61c847ac770c2aff481",
+    "url": "/Ecommerce/static/media/person_2.2cd3dcd3.jpg"
+  },
+  {
+    "revision": "514eee1b06ddf4a5692f7264d3da79aa",
+    "url": "/Ecommerce/static/media/person_3.514eee1b.jpg"
+  },
+  {
+    "revision": "cfb95125014362aa10870999ff5d645d",
+    "url": "/Ecommerce/static/media/person_4.cfb95125.jpg"
+  },
+  {
+    "revision": "963bbd6f3636e7d74108d430dbed9ddf",
+    "url": "/Ecommerce/static/media/recommend.963bbd6f.jpg"
+  }
+]);
